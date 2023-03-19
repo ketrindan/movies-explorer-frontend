d327081e84b763ec7215af7bfc752c4142153eb6
Kate Danilenko <keitilins@yandex.ru>
2023-03-20 00:17:42 +0300

    add InfoTooltip + edit Profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, useHistory  } from 'react-router-dom';
 
 import './App.css';
 
@@ -10,37 +10,72 @@ import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import InfoTooltip from '../InfoTooltip/InfoTooltip';
+
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const [isInfoTooltipOpen, setInfoTooltipOpen] = useState(false);
+  const [infoMessage, setInfoMessage] = useState('')
+  const [isSuccessful, setIsSuccessful] = useState(false);
+
+  const history = useHistory();
+
+  function openInfoTooltip() {
+    setInfoTooltipOpen(true);
+  }
+
+  function closeInfoTooltip() {
+    setInfoTooltipOpen(false);
+    setInfoMessage('');
+  }
+
   return (
     <div className="body">
       <div className="app">
-      <Switch>
-        <Route exact path="/">
-          <Main isLoggedIn={false} />
-        </Route> 
-        <Route path="/movies">
-          <Movies isLoggedIn={true} />
-        </Route> 
-        <Route path="/saved-movies">
-          <SavedMovies isLoggedIn={true} />
-        </Route> 
-        <Route path="/profile">
-          <Profile 
-            isLoggedIn={true} 
+        <InfoTooltip 
+          isOpen={isInfoTooltipOpen}
+          onClose={closeInfoTooltip}
+          isSuccessful={isSuccessful}
+          message={infoMessage}
+        />
+
+        <Switch>
+          <Route exact path="/">
+            <Main isLoggedIn={isLoggedIn} />
+          </Route> 
+          <ProtectedRoute path="/movies"
+            component={Movies}
+            isLoggedIn={isLoggedIn} 
+          /> 
+          <ProtectedRoute path="/saved-movies"
+            component={SavedMovies}
+            isLoggedIn={isLoggedIn} 
+          />
+          <ProtectedRoute path="/profile"
+            component={Profile} 
+            isLoggedIn={isLoggedIn} 
             name={"Катя"}
           />
-        </Route> 
-        <Route path="/signup">
-          <Register />
-        </Route>
-        <Route path="/signin">
-          <Login />
-        </Route>
-        <Route path="*">
-          <NotFoundPage />
-        </Route> 
-       </Switch>
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NotFoundPage />
+          </Route> 
+          <Route>
+            {isLoggedIn ? <Redirect to="/" /> : <Redirect to="/signin" />}
+          </Route>
+        </Switch>
       </div>
     </div>
   )
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import './Profile.css';
 import useForm from "../../hooks/useForm";
 import Header from '../Header/Header';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function Profile(props) {
   const { values, errors, isValid, onChange } = useForm({
@@ -18,25 +19,27 @@ function Profile(props) {
         isAbsolute={true}
       />
       <div className="profile__container">
-        <h1 className="profile__title">Привет, {props.name}!</h1>
+        <h1 className="profile__title">Привет, {values.name}!</h1>
         <form className="profile__form">
           <div className="profile__input-box">
             <label className="profile__label">Имя</label>
             <input className="profile__input" type="text" placeholder="Иван" id="name" name="name"
             value={values.name} onChange={onChange} minLength="2" required/>
           </div>
+          <span className={"profile__error " + (errors.name ? "profile__error_active" : "")}>{errors.name}</span>
           <div className="profile__input-box">
             <label className="profile__label">E-mail</label>
             <input className="profile__input" type="email" placeholder="qwerty@yandex.ru" id="email" name="email"
             value={values.email} onChange={onChange} required/>
           </div>
-        </form>
-        <div className="profile__links">
-          <button className={"profile__btn profile__btn_edit " + (isValid ? "profile__btn_active" : "" )}>Редактировать</button>
+          <span className={"profile__error " + (errors.email ? "profile__error_active" : "")}>{errors.email}</span>
+          <div className="profile__links">
+          <button className={"profile__btn profile__btn_edit " + (isValid ? "profile__btn_active" : "" )} type="submit">Редактировать</button>
           <Link to='/signin'>
             <button className="profile__btn profile__btn_signout">Выйти из аккаунта</button>
           </Link>
         </div>
+        </form>
       </div>
     </section>
   )
